dd9da710855dfe784244fe4afaf46cc62f9acace
Björn Rydahl <bjorn.rydahl@gmail.com>
2025-11-26 12:28:49 +0100

    Add 3.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="page">
       <h1>Hello World</h1>
-      <p>Welcome to the home page! 2</p>
+      <p>Welcome to the home page! 3</p>
       <Link to="/about">
         <button className="nav-button">Go to About Page</button>
       </Link>
