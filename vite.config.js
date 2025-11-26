d0365cd98c4584109f91785ddde03a32567d00ba
Björn Rydahl <bjorn.rydahl@gmail.com>
2025-11-26 12:24:28 +0100

    Add multi support.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/test-pages/',
+  base: process.env.VITE_BASE || '/test-pages/',
 })
 
